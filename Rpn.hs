5fd4fa02732f8bdd862175fac1cd553ae0c46ba9
weifuchuan <fuchuan.wei@hotmail.com>
2018-03-28 17:29:54 +0800

    rpn computer

---
+++ Rpn.hs
@@ -0,0 +1,24 @@
+module Rpn(
+eval
+)where
+
+import qualified Data.Map as Map
+import qualified Data.List as List
+
+eval :: String -> Double
+eval = eval' [] . words
+
+eval' :: [Double] -> [String] -> Double
+eval' = \s expr -> (\(x:[]) -> x) $ List.foldl' (\ stack e -> case Map.lookup e op of
+                                              Just (f, cnt) -> let (h, t) = splitAt cnt stack in (f h):t
+                                              Nothing -> let v = read e :: Double in v:stack
+                                            ) s expr
+
+op :: Map.Map String ([Double]->Double, Int)
+op = Map.fromList [
+  ("+", (\(a:b:[]) -> b+a, 2))
+  , ("-", (\(a:b:[]) -> b-a, 2))
+  , ("*", (\(a:b:[]) -> b*a, 2))
+  , ("/", (\(a:b:[]) -> b/a, 2))]
+
+  
