e619d4794acf598d4bf44d236ac89d3faf1efe78
weifuchuan <fuchuan.wei@hotmail.com>
2018-03-28 17:48:50 +0800

    rpn computer add annotation 2

--- Rpn.hs
+++ Rpn.hs
@@ -2,6 +2,13 @@
 -- example: eval "1 2 +"
 --          eval "1 2 3 * +"
 --          eval "1 2 3 4 5 - + / *"
+-- 你可以扩展他，例如加入++：
+-- op = Map.fromList [... ("++", (\(a:[]) -> a + 1, 1))]
+-- 加入sqrt: op = Map.fromList [... ("sqrt", (\(a:[]) -> sqrt a , 1))]
+-- 如果你希望扩展那些多于1元数的运算符，一定要记住，\args->..的args列表是逆序的，所以有：\(a:b:[]) -> b/a
+-- 例如加入mod: 
+-- op = Map.fromList [... ("mod", (\(a:b:[]) -> mod b a, 2))]
+-- 不幸的是，上面的例子是错的，你需要先将a b转为Int, 再将结果转Double
 
 module Rpn(
 eval
